b1abe3299bca7060b51cff15edc0a13d14faa9bc
nan <nandangsopyan08@gmail.com>
2022-05-29 00:43:57 +0700

    updating count of duplication

--- count_of_duplication_array.rb
+++ count_of_duplication_array.rb
@@ -1,4 +1,15 @@
 class Array
   def count_duplication
+    result = self.inject({}) do |count, item|
+      count[item] ||= 0
+      count[item] += 1
+      count
+    end
+
+    result.each do |key, value|
+      puts "#{key} = #{value}" unless value <= 1
+    end
   end
 end
+
+[4, 2, 2, 8, 5, 1, 2, 4].count_duplication
