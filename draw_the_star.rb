718d29224faba21b5ffc349e33409c098bbcfb1b
nan <nandangsopyan08@gmail.com>
2022-05-28 21:41:05 +0700

    adding drwa a "c" star

--- draw_the_star.rb
+++ draw_the_star.rb
@@ -1,9 +1,11 @@
 def draw_the_star
   star = 0
-  space = ""
+  space = " "
 
+  # this is for draw a 'a' star
   puts "a"
   puts ""
+
   5.times do 
     star += 1
     star.times do
@@ -12,15 +14,34 @@ def draw_the_star
     puts ""
   end
 
+  # this is for draw a 'b' star
   star = 6
-
   puts ""
   puts "b"
+
   5.times do
     star -= 1
     star.times do
       print "*"
     end
+    puts ""
+  end
+
+  # this is for draw a 'c' star
+  star = 5
+  puts ""
+  puts "c"
+
+  5.times do
+    star -= 1
+    star.times do
+      print space
+    end
+    
+    (5-star).times do
+      print "*"
+    end
+
     puts ""
   end
   
