743981f317e537c9f27fcdd9cd34fa837eef3501
nan <nandangsopyan08@gmail.com>
2022-05-28 22:30:26 +0700

    updating fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,13 @@
 def fibonacci
-  initial_value = [0, 1]
-  print initial_value
+  fibonacci_numbers = [0, 1]
+  index = 0
+
+  8.times do
+    fibonacci_numbers.append(fibonacci_numbers[index] + fibonacci_numbers[index + 1])
+    index += 1
+  end
+  print fibonacci_numbers
+
   puts ""
 end
 
