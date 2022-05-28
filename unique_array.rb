a592fe3fea5e083693eed55a89be540d7e51f41a
nan <nandangsopyan08@gmail.com>
2022-05-29 00:37:49 +0700

    adding uniq array

---
+++ unique_array.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+class Array
+  def custom_uniq
+    uniq_array = []
+
+    self.each_with_index do |item, index|
+      uniq_array << item unless uniq_array.include?(item)
+    end
+
+    print uniq_array
+
+  end
+end
+
+[4, 2, 2, 8, 5, 1, 2, 4].custom_uniq
