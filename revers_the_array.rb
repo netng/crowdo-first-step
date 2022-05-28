228d5a9f0e89b3264cabd9dd3f3d3f3abe57267f
nan <nandangsopyan08@gmail.com>
2022-05-28 22:50:15 +0700

    updating revers_the_array

--- revers_the_array.rb
+++ revers_the_array.rb
@@ -1,3 +1,14 @@
 def reverse
+  array_list = [4, 2, 8, 5, 1]
+  reverse_array_list = []
+  length = array_list.size
 
+  length.times do
+    swap_item = array_list.pop
+    reverse_array_list.append(swap_item)
+  end
+
+  print reverse_array_list
 end
+
+reverse
